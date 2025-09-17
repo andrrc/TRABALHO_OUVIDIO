0f06a73773d54b644e7afe40c825af9a8f13d6d0
andrrc <andrevieiracvf@gmail.com>
2025-09-16 21:29:53 -0300

    new hero

--- src/components/Hero.jsx
+++
@@ -1,9 +0,0 @@
-import "./hero.css";
-function Hero(){
-    return(
-        <section>
-            <img src="/estilo-se-conquista.jpg" alt=""  class="hero-img"/>
-        </section>
-    )
-}
-export default Hero;
